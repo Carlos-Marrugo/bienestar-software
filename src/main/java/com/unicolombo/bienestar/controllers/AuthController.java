1fb4aecd2500e59db00449edb7f9ab0db3413d81
unknown <elrprogramadortutoriales@gmail.com>
2025-02-26 15:52:07 -0500

    feat: add AuthController with login endpoint

---
+++ src/main/java/com/unicolombo/bienestar/controllers/AuthController.java
@@ -0,0 +1,25 @@
+package com.unicolombo.bienestar.controllers;
+
+import com.unicolombo.bienestar.services.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+
+public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
+        String token = authService.authenticarUsuario(loginRequest.getCorreo(), loginRequest.getPassword());
+        return ResponseEntity.ok(new AuthResponse(token));
+    }
+
+}
