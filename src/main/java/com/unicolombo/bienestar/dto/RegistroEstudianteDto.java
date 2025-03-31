c423c24473c34c3318e6a5bef894d0e6cb43c79e
Carlos-Marrugo <elrprogramadortutoriales@gmail.com>
2025-03-31 10:40:01 -0500

    solving conflicts

--- src/main/java/com/unicolombo/bienestar/dto/RegistroEstudianteDto.java
+++ src/main/java/com/unicolombo/bienestar/dto/RegistroEstudianteDto.java
@@ -5,18 +5,9 @@ import lombok.Data;
 
 @Data
 public class RegistroEstudianteDto {
-
-    @NotBlank(message = "La contraseña es obligatoria")
-    @Size(min = 8, message = "La contraseña debe tener al menos 8 caracteres")
+    private String email;
     private String password;
-
-    @NotBlank(message = "El nombre es obligatorio")
     private String nombre;
-
-    @NotBlank(message = "El apellido es obligatorio")
     private String apellido;
-
-    @NotBlank(message = "El código estudiantil es obligatorio")
-    @Pattern(regexp = "^UC\\d{6}$", message = "El código debe tener formato UC seguido de 6 dígitos")
     private String codigoEstudiantil;
 }
