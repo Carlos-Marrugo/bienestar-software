98805a5e81b69b1a2c6fd5ccb1959c179760ba8b
Carlos-Marrugo <elrprogramadortutoriales@gmail.com>
2025-03-31 10:51:05 -0500

    se impleto la seguridad para ruta de registro

--- src/main/java/com/unicolombo/bienestar/config/SecurityConfig.java
+++ src/main/java/com/unicolombo/bienestar/config/SecurityConfig.java
@@ -28,6 +28,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/estudiantes/registro").permitAll()
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/api/actividades/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
